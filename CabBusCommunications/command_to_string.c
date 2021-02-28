0ca034655fb96dca4f895936ab2bca38342cf70b
Robert Middleton <robert.middleton@rm5248.com>
2021-02-28 13:34:02 -0500

    Added command to string function

---
+++ CabBusCommunications/command_to_string.c
@@ -0,0 +1,24 @@
+#include "cab_commands.h"
+
+const char* cabbus_command_to_string( int command ){
+	switch( command ){
+	case CAB_CMD_NONE:
+		return "CabCommandNone";
+	case CAB_CMD_SEL_LOCO:
+		return "CabCommandSelectLoco";
+	case CAB_CMD_SWITCH:
+		return "CabCommandSwitch";
+	case CAB_CMD_MACRO:
+		return "CabCommandMacro";
+	case CAB_CMD_RESPONSE:
+		return "CabCommandResponse";
+	case CAB_CMD_SPEED:
+		return "CabCommandSpeed";
+	case CAB_CMD_DIRECTION:
+		return "CabCommandDirection";
+	case CAB_CMD_ESTOP:
+		return "CabCommandESTOP";
+	default:
+		return "UNKNOWN";
+	}
+}
